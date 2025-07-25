7a07179a4e4bd527ee81374ccae7bb095d6efd16
Ricardo Rojas <richiredk@gmail.com>
2025-07-25 10:47:07 -0600

    Commit 1: Chapter 1 - 1.1 Getting Started

---
+++ helloworld.c
@@ -0,0 +1,20 @@
+/*
+My journey Learning C. 
+I have decided to learn C, following the classic version of The C Programming Language (1978), written by Brian Kernighan and Dennis Ritchie.
+I am following the reading of the book in the video (https://www.youtube.com/watch?v=j-_s8f5K30I&t=2954s) tutorial created by freeCodeCamp (https://www.freecodecamp.org/)
+and Charles Severance (Dr. Chuck - https://www.cc4e.com/).
+Thank you all.
+*/
+
+/*
+This is the first C program in the book which is the classic "hello world". This first program witll not work (compile/run) in my linux desktop,in which I am running ubuntu 24.04.2 LTS, because the standard is too old. 
+Modifications are necessary. 
+*/
+
+#include <stdio.h>
+main()
+{
+
+	printf("Hello, world\n");
+}
+
